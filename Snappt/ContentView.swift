577868adf6840c63b7bc18f4203d7924548a185d
Mackenzie Bryant <mackenziebryantk@gmail.com>
2020-05-02 10:36:42 -0500

    Initial Commit

---
+++ Snappt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snappt
+//
+//  Created by Mackenzie Bryant on 5/2/20.
+//  Copyright © 2020 Mackenzie Bryant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
